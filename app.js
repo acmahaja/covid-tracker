309a20393d4cb3a0a1787b129d52e96f622e4909
Anjaney C Mahajan <acmahaja@gmail.com>
2021-06-05 09:57:43 +0200

    Added country files

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 
-const data = require('./public/data.json')
-
+const data = require('./public/data.json');
+const countries = require('./public/country-code.json');
 
 async function getDataSet() {
     try {
@@ -18,20 +18,30 @@ async function getDataSet() {
             console.log(result);
             return result;
         });
-
-
     } catch (e) {
         console.log(e);
     }
 }
 
+app.get('/country/:code', async(req, res) => {
+    const { code } = req.params;
+    if (code.length != 3 || countries[code] === undefined)
+        res.redirect('/');
 
+    res.send(countries[code])
+});
+
+
+app.get('/data', async(req, res) => {
+    res.send(JSON.stringify(data))
+});
 
 app.get('/', async(req, res) => {
+
     res.render('index', { data })
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
