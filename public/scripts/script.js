f3f80f44c75906c28ab2d118b5ea65fbc5050624
Anjaney C Mahajan <acmahaja@gmail.com>
2021-06-17 08:36:45 +0200

    added country dropdown

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -90,10 +90,10 @@ function addcountry(country) {
     data.cases = numberWithCommas(country.cases)
     data.recovered = numberWithCommas(country.recovered)
     data.active = numberWithCommas(country.active)
-    data.todayCases = numberWithCommas(country.todayCases)
+    data.tests = numberWithCommas(country.tests)
     data.deaths = numberWithCommas(country.deaths)
     data.flag = country.countryInfo.flag
-    const card = `<div class="country-card">
+    const card = `<div class="country-card" id="${country.country}">
                     <div class="title gradient_text">
                         ${country.country}
                     </div>
@@ -123,10 +123,10 @@ function addcountry(country) {
                             </div>
                         </div>
                         <div class="other_stats_num gradient_text">
-                            <div class=" mask"></div>
+                            <div class=" test"></div>
                             <div class="number">
-                                <p>New Cases</p>
-                                <p>${data.todayCases}</p>
+                                <p>Total Tests</p>
+                                <p>${data.tests}</p>
                             </div>
                         </div>
                         <div class="other_stats_num gradient_text">
@@ -141,7 +141,17 @@ function addcountry(country) {
     cards[0].innerHTML += card;
 }
 
+function enableCountry(country) {
+    const card = document.getElementById(country.country);
+    card.style.display = "flex";
 
+}
+
+function disableCountry(country) {
+    const card = document.getElementById(country.country);
+    card.style.display = "none";
+
+}
 
 
 
@@ -161,58 +171,73 @@ github[0].onclick = () => {
 
 let countries_data = [];
 
-document.addEventListener("DOMContentLoaded", async() => {
-
-    await fetch('https://disease.sh/v3/covid-19/all')
-        .then(function(response) {
-            if (response.status === 200) {
-                response.json().then(function(result) {
-                    document.getElementById('total_cases').innerText = numberWithCommas(result.cases);
-                    document.getElementById('total_recoveries').innerText = numberWithCommas(result.recovered);
-                    document.getElementById('new_cases').innerText = numberWithCommas(result.todayCases);
-                    document.getElementById('acitve_cases').innerText = numberWithCommas(result.active);
-                });
-            } else {
-                console.err('Looks like there was a problem. Status Code: ' +
-                    response.status);
-            }
-        })
-
-
-    await fetch('https://disease.sh/v3/covid-19/countries')
-        .then(
-            function(response) {
-
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(async function() {
+        await fetch('https://disease.sh/v3/covid-19/all')
+            .then(function(response) {
                 if (response.status === 200) {
-                    response.json().then(function(countries) {
-                        countries.forEach(country => {
-                            addcountry(country);
-                            countries_data.push(country)
-                        });
+                    const animiations = document.getElementsByClassName("lds-ripple");
+                    for (let i = 0; i < animiations.length; i++) {
+                        animiations[i].style.display = "none";
+                        console.log(animiations);
+                    }
+
+
+                    response.json().then(function(result) {
+
+                        document.getElementById('total_cases').innerText = numberWithCommas(result.cases);
+                        document.getElementById('total_recoveries').innerText = numberWithCommas(result.recovered);
+                        document.getElementById('new_cases').innerText = numberWithCommas(result.todayCases);
+                        document.getElementById('acitve_cases').innerText = numberWithCommas(result.active);
                     });
                 } else {
                     console.err('Looks like there was a problem. Status Code: ' +
                         response.status);
                 }
-            }
-        ).then(() => {
-            displayCountries();
-        })
-        .catch(function(err) {
-            console.err('Fetch Error :-S', err);
-        });
+            })
+    }, 800);
+
+    setTimeout(async function() {
+        await fetch('https://disease.sh/v3/covid-19/countries')
+            .then(
+                function(response) {
+
+                    if (response.status === 200) {
+                        const cards = document.getElementsByClassName('cards')[0];
+                        cards.innerHTML = "";
+                        response.json().then(function(countries) {
+                            countries.forEach(country => {
+
+                                addcountry(country);
+                                countries_data.push(country)
+
+                            });
+                        });
+                    } else {
+                        console.err('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                    }
+                }
+            ).then(() => {
+                displayCountries();
+            })
+            .catch(function(err) {
+                console.err('Fetch Error :-S', err);
+            });
+    }, 1500);
+
+
 
 })
 
 
 document.getElementById("filterer").addEventListener("input", () => {
     const cards = document.getElementsByClassName('cards');
-
-    cards[0].innerHTML = "";
     countries_data.forEach((country) => {
         if (country.country.toLowerCase().includes(document.getElementById("filterer").value.toLowerCase())) {
-            addcountry(country)
-
+            enableCountry(country)
+        } else {
+            disableCountry(country)
         }
     })
 })
